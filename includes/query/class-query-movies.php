87c83e9bdcb9e0e4652984861194fc55b94cea61
Charlie Merland <charlie@caercam.org>
2016-10-11 17:22:57 +0200

    Add Query/Movies class a set of result-pages-related functions

--- includes/query/class-query-movies.php
+++ includes/query/class-query-movies.php
@@ -38,6 +38,13 @@ class Movies {
 	private $args;
 
 	/**
+	 * Current page.
+	 * 
+	 * @var    int
+	 */
+	//protected $current = 0;
+
+	/**
 	 * Internal Query.
 	 * 
 	 * @var    WP_Query
@@ -389,5 +396,68 @@ class Movies {
 		}
 
 		return $this->items;
+	}
+
+	/**
+	 * Retrieve total number of result pages.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   int
+	 */
+	public function get_total_pages() {
+
+		return (int) $this->query->max_num_pages;
+	}
+
+	/**
+	 * Retrieve current page number.
+	 * 
+	 * Default if 0, which we convert to 1 to avoid confusions.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   int
+	 */
+	public function get_current_page() {
+
+		$paged = $this->query->get( 'paged' );
+		if ( ! $paged ) {
+			$paged = 1;
+		}
+
+		return $paged;
+	}
+
+	/**
+	 * Calculate previous page number.
+	 * 
+	 * Avoid returning a negative number if result is smaller than 0.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   int
+	 */
+	public function get_previous_page() {
+
+		$previous = $this->get_current_page() - 1;
+
+		return max( 0, $previous );
+	}
+
+	/**
+	 * Retrieve next page number.
+	 * 
+	 * Avoid returning a number greater than total number of pages.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   int
+	 */
+	public function get_next_page() {
+
+		$next = $this->get_current_page() + 1;
+
+		return min( $this->get_total_pages(), $next );
 	}
 }
