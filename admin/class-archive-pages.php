d21e6a0acaf8650065ddc1e5945878c482c2131f
Charlie Merland <charlie@caercam.org>
2016-10-19 14:55:55 +0200

    ArchivePages class: implement Notices + ButterBean_Control_Posts + better logic

--- admin/class-archive-pages.php
+++ admin/class-archive-pages.php
@@ -11,9 +11,6 @@
 
 namespace wpmoly\Admin;
 
-use wpmoly\Core\Loader;
-use wpmoly\Core\PublicTemplate;
-
 /**
  * Provide a tool to manage custom Archive Pages.
  *
@@ -24,18 +21,48 @@ use wpmoly\Core\PublicTemplate;
 class ArchivePages {
 
 	/**
-	 * Grid Post Type metaboxes.
+	 * Current page Post ID.
+	 * 
+	 * @var    int
+	 */
+	private $post_id;
+
+	/**
+	 * Archive pages.
 	 * 
 	 * @var    array
 	 */
-	private $metaboxes = array();
+	private $pages = array();
+
+	/**
+	 * Archive Types.
+	 * 
+	 * @var    array
+	 */
+	private $types = array();
+
+	/**
+	 * Metaboxes Managers.
+	 * 
+	 * @var    array
+	 */
+	private $managers = array();
 
 	/**
 	 * Class constructor.
 	 * 
+	 * Define supported types and ButterBean managers. This runs very early
+	 * in WordPress wp-admin/admin, even before post.php or post-new.php are
+	 * loaded, which means we need to grab $post_id directly from the URL.
+	 * 
 	 * @since    3.0
 	 */
 	public function __construct() {
+
+		// Grap current page ID from URL
+		if ( isset( $_GET['post'] ) ) {
+			$this->post_id = (int) $_GET['post'];
+		}
 
 		$types = array(
 			'movies'      => __( 'Movies', 'wpmovielibrary' ),
@@ -54,8 +81,12 @@ class ArchivePages {
 		 */
 		$this->types = apply_filters( 'wpmoly/filter/archive_pages/types', $types, $this );
 
-		$managers = array(
-			'archive-page-settings' => array(
+		// Load archive pages
+		$this->pages = $this->get_archive_pages();
+
+		// Only register archive pages' manager when needed
+		if ( ! empty( $this->pages[ $this->post_id ] ) ) {
+			$this->managers['archive-page-settings'] = array(
 				'label'     => esc_html__( 'Archive Page Settings', 'wpmovielibrary' ),
 				'post_type' => 'page',
 				'context'   => 'normal',
@@ -66,18 +97,48 @@ class ArchivePages {
 						'icon'  => 'wpmolicon icon-grid',
 						'settings' => array(
 							'grid-id' => array(
-								'type'     => 'text',
+								'type'     => 'posts',
+								'post_type'=> 'grid',
 								'section'  => 'grid-settings',
 								'label'    => esc_html__( 'Grid ID', 'wpmovielibrary' ),
-								'description' => esc_html__( 'Grid to show in the page content.', 'wpmovielibrary' ),
+								'description' => sprintf( esc_html__( 'Select a Grid to show in the page content. %s', 'wpmovielibrary' ), sprintf( '<a href="%s" target="_blank">%s</a>', esc_url( admin_url( 'post-new.php?post_type=grid' ) ), __( 'Create a new one?', 'wpmovielibrary' ) ) ),
 								'attr'     => array( 'class' => 'half-col', 'size' => '4' ),
 								'default'  => ''
+							),
+							'grid-position' => array(
+								'type'     => 'radio',
+								'section'  => 'grid-settings',
+								'label'    => esc_html__( 'Grid Position', 'wpmovielibrary' ),
+								'description' => esc_html__( 'Where should the Grid be displayed.', 'wpmovielibrary' ),
+								//'description' => esc_html__( 'Where should the Grid be displayed. You can include the Grid in a custom place in your content by adding inserting the <code>[archives_grid]</code> in your content.', 'wpmovielibrary' ),
+								'attr'     => array( 'class' => 'half-col' ),
+								'choices'  => array(
+									'top'    => __( 'Before Post content', 'wpmovielibrary' ),
+									'bottom' => __( 'After Post content', 'wpmovielibrary' ),
+									//'custom' => __( 'Custom', 'wpmovielibrary' )
+								),
+								'default'  => 'top'
+							)
+						)
+					),
+					'page-customization' => array(
+						'label' => esc_html__( 'Customization', 'wpmovielibrary' ),
+						'icon'  => 'wpmolicon icon-advanced',
+						'description' => esc_html__( 'Try to customize the page behaviour and appearance.<br /><strong>Note:</strong> this options may be ineffective depending on your theme and other plugins you are running that may interfere.', 'wpmovielibrary' ),
+						'settings' => array(
+							'adapt-page-title' => array(
+								'type'     => 'checkbox',
+								'post_type'=> 'grid',
+								'section'  => 'grid-settings',
+								'label'    => esc_html__( 'Adapt Page Title', 'wpmovielibrary' ),
+								'description' => esc_html__( 'Try to adapt the page’s title to fit the grid content changes: ordering, sorting…', 'wpmovielibrary' ),
+								'attr'     => array( 'class' => 'half-col' )
 							)
 						)
 					)
 				)
-			)
-		);
+			);
+		}
 
 		/**
 		 * Filter archive pages managers for the grid builder.
@@ -87,7 +148,7 @@ class ArchivePages {
 		 * @param    array     $managers Default managers.
 		 * @param    object    ArchivePages instance.
 		 */
-		$this->managers = apply_filters( 'wpmoly/filter/archive_pages/managers', $managers, $this );
+		$this->managers = apply_filters( 'wpmoly/filter/archive_pages/managers', $this->managers, $this );
 	}
 
 	/**
@@ -161,6 +222,7 @@ class ArchivePages {
 						array(
 							'section'     => $section_id,
 							'type'        => isset( $control->type )        ? $control->type        : false,
+							'post_type'   => isset( $control->post_type )   ? $control->post_type   : false,
 							'label'       => isset( $control->label )       ? $control->label       : false,
 							'attr'        => isset( $control->attr )        ? $control->attr        : false,
 							'choices'     => isset( $control->choices )     ? $control->choices     : false,
@@ -195,16 +257,18 @@ class ArchivePages {
 		if ( 'page' !== $post->post_type ) {
 			return false;
 		}
+
+		$page_type = isset( $this->pages[ $post->ID ] ) ? $this->pages[ $post->ID ] : '';
 ?>
 
 		<div class="misc-pub-section archive-page misc-pub-archive-page">
-			<span class="wpmolicon icon-wpmoly"></span><?php _e( 'Type of Archives:', 'wpmovielibrary' ); ?> <span id="wpmoly-archive-page-type"><?php _e( 'None' ); ?></span> <a id="wpmoly-edit-archive-page" href="#"><?php _e( 'Edit' ); ?></a>
+			<span class="wpmolicon icon-wpmoly"></span><?php _e( 'Type of Archives:', 'wpmovielibrary' ); ?> <span id="wpmoly-archive-page-type"><?php echo $page_type ? $this->types[ $page_type ] : __( 'None' ); ?></span> <a id="wpmoly-edit-archive-page" href="#"><?php _e( 'Edit' ); ?></a>
 			<div id="wpmoly-edit-archive-page-type" class="hide-if-js">
 				<p>
 					<select id="wpmoly-archive-page-types" name="wpmoly[archive_page_type]">
 						<option value=""></option>
 <?php foreach ( $this->types as $type => $name ) : ?>
-						<option value="<?php echo esc_attr( $type ); ?>"><?php echo esc_attr( $name ); ?></option>
+						<option value="<?php echo esc_attr( $type ); ?>"<?php selected( $page_type, $type ); ?>><?php echo esc_attr( $name ); ?></option>
 <?php endforeach; ?>
 					</select>
 				</p>
@@ -272,16 +336,83 @@ class ArchivePages {
 			return false;
 		}
 
-		$archive_type = $_POST['wpmoly']['archive_page_type'];
+		$archive = $_POST['wpmoly']['archive_page_type'];
+		$oldpage = array_search( $archive, $this->pages );
 
-		$archive_pages = get_option( '_wpmoly_archive_pages' );
-		if ( ! $archive_pages ) {
-			$archive_pages = array_map( '__return_zero', array_keys( $this->types ) );
+		// Same page, nothing to change
+		if ( $oldpage == $post_id ) {
+			return false;
 		}
 
-		$archive_pages[ $archive_type ] = $post_id;
+		// Archive page already set for this type
+		if ( $oldpage && $oldpage !== $post_id ) {
+			unset( $this->pages[ $oldpage ] );
+		}
 
-		set_option( '_wpmoly_archive_pages', $archive_pages );
+		$this->pages[ $post_id ] = $archive;
+
+		$this->set_notice();
+		$this->set_archive_pages();
+	}
+
+	/**
+	 * Retrieve current archive pages from options.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   array
+	 */
+	private function get_archive_pages() {
+
+		if ( ! empty( $this->pages ) ) {
+			return $this->pages;
+		}
+
+		$pages = get_option( '_wpmoly_archive_pages' );
+		if ( ! $pages ) {
+			$pages = array();
+		}
+
+		return $this->pages = $pages;
+	}
+
+	/**
+	 * Update archive pages option.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   array
+	 */
+	private function set_archive_pages() {
+
+		$pages = $this->get_archive_pages();
+
+		return update_option( '_wpmoly_archive_pages', $pages );
+	}
+
+	/**
+	 * Changes have been made, permalinks need to be updated: set a transient
+	 * to trigger an admin notice.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   array
+	 */
+	private function set_notice() {
+
+		return set_transient( '_wpmoly_reset_permalinks_notice', 1 );
+	}
+
+	/**
+	 * Remove admin notice transient.
+	 * 
+	 * @since    3.0
+	 * 
+	 * @return   array
+	 */
+	private function remove_notice() {
+
+		return delete_transient( '_wpmoly_reset_permalinks_notice' );
 	}
 
 }
