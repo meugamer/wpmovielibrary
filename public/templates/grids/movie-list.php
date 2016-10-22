95ed131274421cd60e4ae97b7a82b1b929f54762
Charlie Merland <charlie@caercam.org>
2016-10-22 12:06:57 +0200

    Grid 'list' and 'archive' modes update

--- public/templates/grids/movie-list.php
+++ public/templates/grids/movie-list.php
@@ -12,10 +12,12 @@
 ?>
 
 	<div class="wpmoly shortcode movies grid list theme-<?php echo $grid->theme; ?>">
+<?php if ( $grid->show_menu() ) : ?>
 		<div class="grid-menu clearfix">
 			<button type="button" data-action="grid-menu" class="button left"><span class="wpmolicon icon-order"></span></button>
 			<button type="button" data-action="grid-settings" class="button right"><span class="wpmolicon icon-settings"></span></button>
 		</div>
+<?php endif; ?>
 		<div class="grid-content list clearfix">
 
 <?php if ( $items->has_items() ) : ?>
@@ -29,9 +31,19 @@
 			</ul>
 <?php endif; ?>
 		</div>
+<?php if ( $grid->show_pagination() ) : ?>
 		<div class="grid-menu pagination-menu clearfix">
-			<button type="button" data-action="grid-paginate" data-value="prev" class="button left"><span class="wpmolicon icon-arrow-left"></span></button>
-			<div class="pagination-menu">Page <span class="current-page"><input type="text" size="1" data-action="grid-paginate" value="1" /></span> of <span class="total-pages">123</span></div>
-			<button type="button" data-action="grid-paginate" data-value="next" class="button right"><span class="wpmolicon icon-arrow-right"></span></button>
+<?php if ( ! $grid->is_first_page() ) : ?>
+			<a href="<?php echo esc_url( $grid->get_previous_page_url() ); ?>" data-action="grid-paginate" data-value="prev" class="button left"><span class="wpmolicon icon-arrow-left"></span></a>
+<?php else : ?>
+			<a class="button left disabled"><span class="wpmolicon icon-arrow-left"></span></a>
+<?php endif; ?>
+			<div class="pagination-menu">Page <span class="current-page"><input type="text" size="1" data-action="grid-paginate" value="<?php echo esc_attr( $grid->get_current_page() ); ?>" /></span> of <span class="total-pages"><?php echo esc_attr( $grid->get_total_pages() ); ?></span></div>
+<?php if ( ! $grid->is_last_page() ) : ?>
+			<a href="<?php echo esc_url( $grid->get_next_page_url() ); ?>" data-action="grid-paginate" data-value="next" class="button right"><span class="wpmolicon icon-arrow-right"></span></a>
+<?php else : ?>
+			<a class="button right disabled"><span class="wpmolicon icon-arrow-right"></span></a>
+<?php endif; ?>
 		</div>
+<?php endif; ?>
 	</div>
