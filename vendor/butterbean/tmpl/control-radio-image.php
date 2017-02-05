be07a41e73171dd29752f201af1c22213f3f815b
Charlie Merland <charlie@talyes.in>
2017-02-05 04:51:38 +0100

    Grid Builder / ButterBean radio image design tweak

--- vendor/butterbean/tmpl/control-radio-image.php
+++ vendor/butterbean/tmpl/control-radio-image.php
@@ -8,7 +8,7 @@
 
 <# _.each( data.choices, function( args, choice ) { #>
 
-	<label>
+	<label title="{{ args.label }}">
 		<input type="radio" value="{{ choice }}" name="{{ data.field_name }}" <# if ( data.value === choice ) { #> checked="checked" <# } #> />
 		<span class="screen-reader-text">{{ args.label }}</span>
 		<img src="{{ args.url }}" alt="{{ args.label }}" />
