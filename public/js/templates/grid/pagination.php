9601594bb4e4803fbbded30e5921edfd6c878999
Charlie Merland <charlie@talyes.in>
2017-02-22 18:55:15 +0100

    Backbone grid template missing translation fix

--- public/js/templates/grid/pagination.php
+++ public/js/templates/grid/pagination.php
@@ -1,12 +1,12 @@
 
 <# if ( 1 < data.state.get( 'currentPage' ) ) { #>
-			<button data-action="grid-navigate" data-value="prev" class="button left"><span class="wpmolicon icon-arrow-left"></span></button>
+			<button data-action="grid-navigate" data-value="prev" class="button left" title="<?php _e( 'Previous Page', 'wpmovielibrary' ); ?>"><span class="wpmolicon icon-arrow-left"></span></button>
 <# } else { #>
 			<button class="button left disabled"><span class="wpmolicon icon-arrow-left"></span></button>
 <# } #>
-			<div class="pagination-menu">Page <span class="current-page"><input type="text" size="1" data-action="grid-paginate" value="{{ data.state.get( 'currentPage' ) }}" /></span> of <span class="total-pages">{{ data.state.get( 'totalPages' ) }}</span></div>
+			<div class="pagination-menu"><?php _e( 'Page', 'wpmovielibrary' ); ?> <span class="current-page"><input type="text" size="1" data-action="grid-paginate" value="{{ data.state.get( 'currentPage' ) }}" /></span> <?php _e( 'of', 'wpmovielibrary' ); ?> <span class="total-pages">{{ data.state.get( 'totalPages' ) }}</span></div>
 <# if ( data.state.get( 'currentPage' ) < data.state.get( 'totalPages' ) ) { #>
-			<button data-action="grid-navigate" data-value="next" class="button right"><span class="wpmolicon icon-arrow-right"></span></button>
+			<button data-action="grid-navigate" data-value="next" class="button right" title="<?php _e( 'Next Page', 'wpmovielibrary' ); ?>"><span class="wpmolicon icon-arrow-right"></span></button>
 <# } else { #>
 			<button class="button right disabled"><span class="wpmolicon icon-arrow-right"></span></button>
 <# } #>
--- public/js/templates/grid/settings.php
+++ public/js/templates/grid/settings.php
@@ -21,10 +21,10 @@ foreach ( $letters as $letter ) {
 		<div class="grid-setting-block half-col orderby-setting">
 			<span class="grid-setting-label"><?php _e( 'Order by:', 'wpmovielibrary' ); ?></span>
 			<span class="grid-setting-input">
-				<input id="{{ data.grid_id }}-orderby-post_title" name="{{ data.grid_id }}-orderby[]" type="radio" data-setting-type="orderby" value="title" <# if ( 'title' == data.query.get( 'orderby' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-orderby-title" class="value"><?php _e( 'Title', 'wpmovielibrary' ); ?></label>
+				<input id="{{ data.grid_id }}-orderby-title" name="{{ data.grid_id }}-orderby[]" type="radio" data-setting-type="orderby" value="title" <# if ( 'title' == data.query.get( 'orderby' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-orderby-title" class="value"><?php _e( 'Title', 'wpmovielibrary' ); ?></label>
 			</span>
 			<span class="grid-setting-input">
-				<input id="{{ data.grid_id }}-orderby-post_date" name="{{ data.grid_id }}-orderby[]" type="radio" data-setting-type="orderby" value="date" <# if ( 'date' == data.query.get( 'orderby' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-orderby-date" class="value"><?php _e( 'Date', 'wpmovielibrary' ); ?></label>
+				<input id="{{ data.grid_id }}-orderby-date" name="{{ data.grid_id }}-orderby[]" type="radio" data-setting-type="orderby" value="date" <# if ( 'date' == data.query.get( 'orderby' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-orderby-date" class="value"><?php _e( 'Date', 'wpmovielibrary' ); ?></label>
 			</span>
 		</div>
 <# 	} #>
@@ -32,10 +32,10 @@ foreach ( $letters as $letter ) {
 		<div class="grid-setting-block half-col order-setting">
 			<span class="grid-setting-label"><?php _e( 'Order:', 'wpmovielibrary' ); ?></span>
 			<span class="grid-setting-input">
-				<input id="{{ data.grid_id }}-order-asc" name="{{ data.grid_id }}-order[]" type="radio" data-setting-type="order" value="asc" <# if ( 'asc' == data.query.get( 'order' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-order-asc" class="value"><?php _e( 'Ascendingly' ); ?></label>
+				<input id="{{ data.grid_id }}-order-asc" name="{{ data.grid_id }}-order[]" type="radio" data-setting-type="order" value="asc" <# if ( 'asc' == data.query.get( 'order' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-order-asc" class="value"><?php _e( 'Ascendingly', 'wpmovielibrary' ); ?></label>
 			</span>
 			<span class="grid-setting-input">
-				<input id="{{ data.grid_id }}-order-desc" name="{{ data.grid_id }}-order[]" type="radio" data-setting-type="order" value="desc" <# if ( 'desc' == data.query.get( 'order' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-order-desc" class="value"><?php _e( 'Descendingly' ); ?></label>
+				<input id="{{ data.grid_id }}-order-desc" name="{{ data.grid_id }}-order[]" type="radio" data-setting-type="order" value="desc" <# if ( 'desc' == data.query.get( 'order' ) ) { #>checked="checked" <# } #>/><label for="{{ data.grid_id }}-order-desc" class="value"><?php _e( 'Descendingly', 'wpmovielibrary' ); ?></label>
 			</span>
 		</div>
 <# } #>
