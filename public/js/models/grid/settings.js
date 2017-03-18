2806f5307977c956088b26cd879bf62198a0e713
Charlie Merland <charlie@talyes.in>
2017-03-18 03:31:30 +0100

    Update Backbone grid settings defaults

--- public/js/models/grid/settings.js
+++ public/js/models/grid/settings.js
@@ -6,22 +6,26 @@ _.extend( wpmoly.model, {
 	Settings: Backbone.Model.extend({
 
 		defaults: {
-			type            : '',
-			mode            : 'grid',
-			theme           : 'default',
-			preset          : '',
-			columns         : 5,
-			rows            : 4,
-			list_columns    : 3,
-			list_rows       : 8,
-			column_width    : 160,
-			row_height      : 240,
-			show_menu       : 1,
-			mode_control    : 0,
-			content_control : 0,
-			display_control : 0,
-			order_control   : 1,
-			show_pagination : 1
+			type              : '',
+			mode              : 'grid',
+			theme             : 'default',
+			preset            : '',
+			columns           : 5,
+			rows              : 4,
+			column_width      : 160,
+			row_height        : 240,
+			list_columns      : 3,
+			list_column_width : 240,
+			list_rows         : 8,
+			enable_ajax       : 1,
+			enable_pagination : 1,
+			settings_control  : 1,
+			custom_letter     : 1,
+			custom_order      : 1,
+			customs_control   : 0,
+			custom_mode       : 0,
+			custom_content    : 0,
+			custom_display    : 0
 		},
 
 		/**
@@ -36,11 +40,19 @@ _.extend( wpmoly.model, {
 		 */
 		initialize: function( attributes, options ) {
 
-			var options = options || {};
+			var options = options || {},
+			   booleans = [ 'enable_ajax', 'enable_pagination', 'settings_control', 'custom_letter', 'custom_order', 'customs_control', 'custom_mode', 'custom_content', 'custom_display' ],
+			   integers = [ 'columns', 'rows', 'column_width', 'row_height', 'list_columns', 'list_column_width', 'list_rows' ];
 
 			_.each( attributes, function( value, key ) {
 				if ( _.isNull( value ) && _.has( this.defaults, key ) ) {
-					this.set( key, this.defaults[ key ], { silent : true } )
+					this.set( key, this.defaults[ key ], { silent : true } );
+				} else if ( ! _.isNull( value ) ) {
+					if ( _.contains( booleans, key ) ) {
+						this.set( key, !! value, { silent : true } );
+					} else if ( _.contains( integers, key ) ) {
+						this.set( key, parseInt( value ), { silent : true } );
+					}
 				}
 			}, this );
 		}
