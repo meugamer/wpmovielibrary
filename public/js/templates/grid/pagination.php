6321d11f2a2c0365d0ef59e2017095ffe42002f3
Charlie Merland <charlie@caercam.org>
2017-08-05 00:09:52 +0200

    Grid pagination tweak: don't show current page input on single paged grids

--- public/js/templates/grid/pagination.php
+++ public/js/templates/grid/pagination.php
@@ -4,7 +4,7 @@
 <# } else { #>
 			<button type="button" disabled="disabled" class="button left"><span class="wpmolicon icon-arrow-left"></span></button>
 <# } #>
-			<div class="pagination-menu"><span class="page-label"><?php _e( 'Page', 'wpmovielibrary' ); ?></span><span class="current-page"><input type="text" size="1" data-action="grid-paginate" value="{{ data.current }}" <# if ( data.preview ) { #>readonly="true" <# } #>/></span><span class="of-label"><?php _e( 'of', 'wpmovielibrary' ); ?></span><span class="total-pages">{{ data.total }}</span></div>
+			<div class="pagination-menu"><span class="page-label"><?php _e( 'Page', 'wpmovielibrary' ); ?></span><span class="current-page"><# if ( 1 < data.total ) { #><input type="text" size="1" data-action="grid-paginate" value="{{ data.current }}" <# if ( data.preview ) { #>readonly="true" <# } #>/><# } else { #>{{ data.current }}<# } #></span><span class="of-label"><?php _e( 'of', 'wpmovielibrary' ); ?></span><span class="total-pages">{{ data.total }}</span></div>
 <# if ( data.current < data.total ) { #>
 			<button type="button" data-action="grid-navigate" data-value="next" class="button right" title="<?php _e( 'Next Page', 'wpmovielibrary' ); ?>"><span class="wpmolicon icon-arrow-right"></span></button>
 <# } else { #>
